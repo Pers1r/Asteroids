9ddddeae910d7b89dad151adcee47dba6a43b3f2
Pers1r <Marek007.nowicki007@gmail.com>
2025-09-05 15:28:28 +0200

    Added pygame initialisition and default game loop

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import pygame
+import constants
+from constants import *
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
