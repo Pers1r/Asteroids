765cda3b7fbcf962b757b62949835a6467ef2e11
Pers1r <Marek007.nowicki007@gmail.com>
2025-09-06 18:17:11 +0200

    Add player movement

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ def main():
 
     while True:
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60)/1000
+        dt = clock.tick(200)/1000
 
 
 if __name__ == "__main__":
