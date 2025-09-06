d2093167340984edb5af909c991d24567798db2f
Pers1r <Marek007.nowicki007@gmail.com>
2025-09-06 17:56:27 +0200

    Add circleshape file for basic shape class and added clock

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pygame
 import constants
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
-
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
